d820ebf09201443c016914930b9de6960ea576d3
aouzounis <aouzounis@UKEDMXDVLWAOUZ.gfi.com>
2015-02-01 16:26:55 +0000

    tweak status actions job main appearance

--- src/main/resources/jenkins/plugins/svnmerge/IntegrationStatusAction/jobMain.groovy
+++ src/main/resources/jenkins/plugins/svnmerge/IntegrationStatusAction/jobMain.groovy
@@ -3,12 +3,15 @@ import lib.JenkinsTagLib
 //  Show the upstream project
 def t = namespace(JenkinsTagLib.class)
 
-h2("Subversion Integration Tracking")
+h2("Subversion Merge Tracking")
 p(style:"margin-left:1em") {
     text(_("This project is a feature branch of "))
     t.jobLink(job:my.branchProperty.upstreamProject)
-    if(my.lastIntegratedBuild!=null){
- 	   	text(_("Last Integration:  "))
-    	t.buildLink(jobName:my.branchProperty.upstream, job:my.branchProperty.upstreamProject, number:my.lastIntegratedBuild.number)
+}
+
+if(my.lastIntegratedBuild!=null){
+	p(style:"margin-left:1em") {
+		text(_("Last Integration:  "))
+	   	t.buildLink(jobName:my.branchProperty.upstream, job:my.branchProperty.upstreamProject, number:my.lastIntegratedBuild.number)
 	}
 }
--- src/main/resources/jenkins/plugins/svnmerge/RebaseStatusAction/jobMain.groovy
+++ src/main/resources/jenkins/plugins/svnmerge/RebaseStatusAction/jobMain.groovy
@@ -3,12 +3,9 @@ import lib.JenkinsTagLib
 //  Show the upstream project
 def t = namespace(JenkinsTagLib.class)
 
-h2("Subversion Rebase Tracking")
-p(style:"margin-left:1em") {
-    text(_("This project is a feature branch of "))
-    t.jobLink(job:my.branchProperty.upstreamProject)
-    if(my.lastRebasedBuild!=null){
- 	   	text(_("Last Rebase:  "))
-    	t.buildLink(jobName:my.branchProperty.upstream, job:my.branchProperty.upstreamProject, number:my.lastRebasedBuild.number)
+if(my.lastRebasedBuild!=null){
+	p(style:"margin-left:1em") {
+		text(_("Last Rebase:  "))
+		t.buildLink(jobName:my.branchProperty.upstream, job:my.branchProperty.upstreamProject, number:my.lastRebasedBuild.number)
 	}
 }
