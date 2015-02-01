4621c53ed9eed4d055f9356df4ef6af183e56133
aouzounis <aouzounis@UKEDMXDVLWAOUZ.gfi.com>
2015-02-01 13:35:50 +0000

    updated rebase action summary.groovy to be more informative

--- src/main/resources/jenkins/plugins/svnmerge/RebaseAction/summary.groovy
+++ src/main/resources/jenkins/plugins/svnmerge/RebaseAction/summary.groovy
@@ -8,7 +8,13 @@ h2 (style:"margin-top:2em", _("Result of Rebase:"))
 if (my.integratedRevision>0){
 	tex(_("UpSteam revision rebased from: ${my.revisionToIntegrateFrom}"))
 	tex(_("Integrated revision          : ${my.integratedRevision}"))			
-}else{
+}
+
+if (my.integratedRevision==-1){
 	tex(_("Rebase failed due to a merge conflict."))
 }
+        
+ if (my.integratedRevision==0){
+ 	tex(_("Nothing to rebase."))
+ }
         
