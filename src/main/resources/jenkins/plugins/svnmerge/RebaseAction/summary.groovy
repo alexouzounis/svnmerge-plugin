7ed105d22d1920aa3af58dcca3c6958b80a77fa0
aouzounis <aouzounis@UKEDMXDVLWAOUZ.gfi.com>
2015-02-01 14:14:39 +0000

    tweak rebase action summary

--- src/main/resources/jenkins/plugins/svnmerge/RebaseAction/summary.groovy
+++ src/main/resources/jenkins/plugins/svnmerge/RebaseAction/summary.groovy
@@ -3,14 +3,6 @@ package jenkins.plugins.svnmerge.RebaseAction
 def t = namespace(lib.JenkinsTagLib.class)
 
 // Rebase is complete. Display the record.
-
-t.summary(icon:"/plugin/svnmerge/48x48/integrate.gif") {
-    text(_("This build is integrated into "))
-    t.buildLink(jobName:my.property.upstream, job:my.property.upstreamProject, number:n)
-}
-
-h2 (style:"margin-top:2em", _("Result of Rebase:"))
-
 if (my.integratedRevision>0){
 	t.summary(icon:"/plugin/svnmerge/48x48/sync.gif") {
 		tex(_("UpSteam revision rebased from: ${my.revisionToIntegrateFrom}"))
