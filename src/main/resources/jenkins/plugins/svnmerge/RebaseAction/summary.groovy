21fc2bb4233089b5100161de40ee44a5a7bc2fc3
aouzounis <aouzounis@UKEDMXDVLWAOUZ.gfi.com>
2015-02-01 14:13:33 +0000

    tweak rebase action summary

--- src/main/resources/jenkins/plugins/svnmerge/RebaseAction/summary.groovy
+++ src/main/resources/jenkins/plugins/svnmerge/RebaseAction/summary.groovy
@@ -3,18 +3,30 @@ package jenkins.plugins.svnmerge.RebaseAction
 def t = namespace(lib.JenkinsTagLib.class)
 
 // Rebase is complete. Display the record.
+
+t.summary(icon:"/plugin/svnmerge/48x48/integrate.gif") {
+    text(_("This build is integrated into "))
+    t.buildLink(jobName:my.property.upstream, job:my.property.upstreamProject, number:n)
+}
+
 h2 (style:"margin-top:2em", _("Result of Rebase:"))
 
 if (my.integratedRevision>0){
-	tex(_("UpSteam revision rebased from: ${my.revisionToIntegrateFrom}"))
-	tex(_("Integrated revision          : ${my.integratedRevision}"))			
+	t.summary(icon:"/plugin/svnmerge/48x48/sync.gif") {
+		tex(_("UpSteam revision rebased from: ${my.revisionToIntegrateFrom}"))
+		tex(_("Integrated revision          : ${my.integratedRevision}"))	
+	}		
 }
 
 if (my.integratedRevision==-1){
-	tex(_("Rebase failed due to a merge conflict."))
+	t.summary(icon:"/plugin/svnmerge/48x48/sync.gif") {
+		tex(_("Failed to rebase due to a merge conflict"))
+	}
 }
         
- if (my.integratedRevision==0){
- 	tex(_("Nothing to rebase."))
- }
+if (my.integratedRevision==0){
+	t.summary(icon:"/plugin/svnmerge/48x48/sync.gif") {
+		tex(_("Nothing to rebase"))
+	}
+}
         
