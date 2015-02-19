65e136ee13a2cd056f60d8a5db1fd1f5cee27128
Alex Ouzounis <alexandros.ouzounis@logicnow.com>
2015-02-19 13:06:12 +0000

    fix bug in rebasestatus summary

--- src/main/resources/jenkins/plugins/svnmerge/RebaseAction/summary.groovy
+++ src/main/resources/jenkins/plugins/svnmerge/RebaseAction/summary.groovy
@@ -7,16 +7,10 @@ def t = namespace(lib.JenkinsTagLib.class)
 // Rebase is complete. Display the record.
 if (my.rebaseRevision!=null && my.rebaseRevision>0){
 	t.summary(icon:"/plugin/svnmerge/48x48/sync.gif") {
-		p {
-			text(_("Rebase from     :"))
-			a(href: Functions.getRelativeLinkTo(my.project), my.project.name)
-		}
-		p {
-			text(_("Rebase source   : ${my.rebaseSource}"))
-		}
-		p {
-			text(_("Rebase revision : ${my.rebaseRevision}"))	
-		}
+		text(_("Rebase from     :"))
+		a(href: Functions.getRelativeLinkTo(my.project), my.project.name)
+		text(_("	Rebase source   : ${my.rebaseSource}"))
+		text(_("	Rebase revision : ${my.rebaseRevision}"))	
 	}		
 }
 
